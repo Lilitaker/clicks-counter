52ce76ee2ad64d8269a5583ca4afdc6d857e84c5
Liliana Vasquez <lilitakerloki@gmail.com>
2022-03-28 20:49:31 -0500

    Add logo to the app (with styles)  and create button component. Add two of this in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
+import Button from './components/Button';
+import counterLogo from './images/click-counter-logo.png'
 
 function App() {
+
+  const manageClick = () => {
+    console.log('Click');
+  }
+
+  const restartCounter = () => {
+    console.log('Restart');
+  }
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='logo-container'>
+        <img className='click-logo' src={counterLogo} alt='Clicks counter logo'/>
+      </div>
+      <div className='main-container'>
+        <Button 
+          text='Click' 
+          isClickBtn={true}
+          manageClick={manageClick}/>
+        <Button
+          text='Restart'
+          isClickBtn={false}
+          manageClick={restartCounter}/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/* 
+Step 2:
+- Import Button component
+- Add two Button components to the App
+- Add text and isClickBtn with their values
+- Create manageClick and restartCounter functions
+- Add manageClick with the functions above as values (the name of the attribute and functions can be different)
+*/
---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//destructuring -> props
+function Button({ text, isClickBtn, manageClick }) { 
+ return (
+  <button 
+    className={ isClickBtn ? "click-btn" : "restart-btn" }
+    onClick={manageClick}>
+    {text}
+  </button>
+ );
+}
+
+export default Button;
+
+/* 
+Step 1: 
+- Create the Button component
+- Add the class and the event
+- Instead of props, we use destructuring to include three parameters 
+- manageClick is a function
+*/
